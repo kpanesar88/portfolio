0f1282faaabf2de6c4da704253466d913a4c149c
Karanveer Panesar <Karanveerpanesar04@gmail.com>
2024-12-28 21:37:09 -0500

    Your commit message

--- app/page_one/interests/interest.tsx
+++ app/page_one/interests/interest.tsx
@@ -7,37 +7,47 @@ const Interests: React.FC = () => {
       <div className="accordion_gallery">
         <div className="item item1">
           <div className="content">
-            <h1 className="title">Image 1</h1>
-            <p className="description">This is some random text under Image 1 that will appear when you hover.</p>
+            <h1 className="title">Snowboarding</h1>
+          </div>
+          <div className="input-box">
+          I've recently started snowboarding and love the adrenaline rush. It's a thrilling challenge, and I'm excited to keep improving and pushing my limits.
           </div>
         </div>
         <div className="item item2">
           <div className="content">
-            <h1 className="title">Image 2</h1>
-            <p className="description">This is some random text under Image 2 that will appear when you hover.</p>
+            <h1 className="title">Hockey</h1>
+          </div>
+          <div className="input-box">
+          I've been a hockey fan since childhood, proudly supporting the Toronto Maple Leafs. Patrick Kane is my favorite player, admired for his incredible hands and skill on the ice.
           </div>
         </div>
         <div className="item item3">
           <div className="content">
-            <h1 className="title">Image 3</h1>
-            <p className="description">This is some random text under Image 3 that will appear when you hover.</p>
+            <h1 className="title">Volleyball</h1>
+          </div>
+          <div className="input-box">
+          I've been playing volleyball since middle school, where I played as a middle blocker during high school games. Recently, I've been expanding my skills by learning how to play as a winger!
           </div>
         </div>
         <div className="item item4">
           <div className="content">
-            <h1 className="title">Image 4</h1>
-            <p className="description">This is some random text under Image 4 that will appear when you hover.</p>
+            <h1 className="title">Video Games</h1>
+          </div>
+          <div className="input-box">
+          I enjoy a variety of video games, from competitive titles like League of Legends, Valorant, and Apex Legends to immersive story-driven games like Red Dead Redemption 2 and A Plague Tale, both of which are among my favorites.
           </div>
         </div>
         <div className="item item5">
           <div className="content">
-            <h1 className="title">Image 5</h1>
-            <p className="description">This is some random text under Image 5 that will appear when you hover.</p>
+            <h1 className="title">Windbreaker</h1>
+          </div>
+          <div className="input-box">
+            Donec sit amet sapien sed libero commodo scelerisque.
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Interests;
