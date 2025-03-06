29d4b018c0e693cc2e0e01a2aedf6f8379dfc61a
Karanveer Panesar <Karanveerpanesar04@gmail.com>
2025-03-06 18:15:08 -0500

    a

--- app/page_one/moreinfo/moreinfo.tsx
+++ app/page_one/moreinfo/moreinfo.tsx
@@ -3,20 +3,18 @@ import './moreinfo.css';
 
 const MoreInfo: React.FC = () => {
   return (
-    <div className="moreinfo-container">
-      {/* Left Side Text */}
-      <div className="moreinfo-left">
-        <h1>Want to learn more?</h1>
-      </div>
-
-      {/* Right Side Cards */}
-      <div className="moreinfo-right">
-        <div className="card">Skills</div>
-
-        <div className="card">Projects</div>
-        
-        <div className="card">Contact</div>
-
+    <div className="more-info-container">
+      <h1>Want to learn more?</h1>
+      <div className="cards-container">
+        <div className="card">
+          <h2>Skills</h2>
+        </div>
+        <div className="card">
+          <h2>Projects</h2>
+        </div>
+        <div className="card">
+          <h2>Contact</h2>
+        </div>
       </div>
     </div>
   );
