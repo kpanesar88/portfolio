584ee6905bb3f8a23407a66d476dd2c0e7da629f
Karanveer Panesar <Karanveerpanesar04@gmail.com>
2025-03-31 14:08:16 -0400

    contact page debug

--- app/projects/page.tsx
+++
@@ -1,9 +0,0 @@
-import Projects from "../page-components/projectspage-components/projects";
-
-export default function ProjectsPage() {
-    return (
-      <div>
-       <Projects />
-      </div>
-    );
-  }
