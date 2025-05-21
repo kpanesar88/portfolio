f2599d08b9b95431151ba62036bdfa980e047340
Karanveer Panesar <Karanveerpanesar04@gmail.com>
2025-05-21 19:41:24 -0400

    favicon fix

--- app/footer/footer.tsx
+++ app/footer/footer.tsx
@@ -4,22 +4,15 @@ import "./footer.css";
 import Head from "next/head";
 
 const Footer: React.FC = () => {
-  // Option 1: Dynamic favicon (runs when component mounts)
-  useEffect(() => {
-    const favicon = document.querySelector("link[rel='icon']") || document.createElement('link');
-    favicon.setAttribute('rel', 'icon');
-    favicon.setAttribute('href', '/images/favicon.ico');
-    document.head.appendChild(favicon);
-  }, []);
 
   return (
     <>
       {/* Option 2: Using Next/Head (alternative approach) */}
       <Head>
-        <link rel="icon" href="/images/favicon.png" />
+        <link rel="icon" href="/images/homepage/favicon.png" />
         {/* Additional favicon formats for better compatibility */}
-        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/homepage/favicon.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/homepage/favicon.png" />
       </Head>
 
       {/* About section here */}
